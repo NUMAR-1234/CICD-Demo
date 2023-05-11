82250cd648b36a544fa5a87212edcccf65feb1ac
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2023-05-11 15:44:45 +0530

    m

--- OWASP-Dependencycheck.sh
+++ OWASP-Dependencycheck.sh
@@ -2,7 +2,7 @@
 
 pwd
 ls -la
-wget https://github.com/jeremylong/DependencyCheck/releases/download/v8.2.1/dependency-check-8.2.1-release.zip
+curl https://github.com/jeremylong/DependencyCheck/releases/download/v8.2.1/dependency-check-8.2.1-release.zip
 unzip dependency-check-8.2.1-release.zip
 ls
 cd dependency-check/bin
