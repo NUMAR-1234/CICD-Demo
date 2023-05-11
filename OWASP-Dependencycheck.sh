23c6d27c6a8b181e86dac510f6d7342d4511355c
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2023-05-11 15:43:24 +0530

    sdc

--- OWASP-Dependencycheck.sh
+++ OWASP-Dependencycheck.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 
-OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
-DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
-REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
-
-if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
-    mkdir -p "$DATA_DIRECTORY"
-    chmod -R 777 "$DATA_DIRECTORY"
-
-    mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
-fi
-
-# Make sure we are using the latest version
-
 pwd
 ls -la
-docker pull owasp/dependency-check
-
-docker run owasp/dependency-check --scan C:/Users/sunda/.jenkins/workspace/CICD pipeline for GitOps/CICD-Demo --format "ALL" --project "sample" --out /report
+wget https://github.com/jeremylong/DependencyCheck/releases/download/v8.2.1/dependency-check-8.2.1-release.zip
+unzip dependency-check-8.2.1-release.zip
+ls
+cd dependency-check/bin
+bash dependency-check
