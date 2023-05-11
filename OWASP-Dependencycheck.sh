3ffaad7c85b97fe7926a470922a549ef4e67d073
NUMAR-1234 <113420565+NUMAR-1234@users.noreply.github.com>
2023-05-11 14:41:29 +0530

    fdv

--- OWASP-Dependencycheck.sh
+++ OWASP-Dependencycheck.sh
@@ -1,7 +1,7 @@
 docker pull owasp/dependency-check
 
 docker run  owasp/dependency-check \
-    --scan / package.json \
+    --scan  package.json \
     --format "ALL" \
     --project "My OWASP Dependency Check Project" \
     --out /report
